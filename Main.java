16ae8252a596ac86f12d83236938a101844f51da
adammosaad03 <mosaadadam647@gmail.com>
2025-02-12 13:56:51 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+  public static void main(String[] args){
+    Bank bankOfGods = new Bank();
+    System.out.println(bankOfGods.accountOne.getBalance());
+    bankOfGods.accountOne.setBalance(5000);
+    System.out.println(bankOfGods.accountOne.getBalance());
+    System.out.println(bankOfGods.accountOne.getMonthlyInterest());
+    bankOfGods.accountOne.closeAccount();
+    System.out.println(bankOfGods.accountOne.getBalance());
+    
+  }
+}
